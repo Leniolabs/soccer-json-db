d00877104054ad591e4c325c09e9932c761d22b4
Manuel Carrizo <manuel.carrizo@leniolabs.com>
2021-10-15 12:45:19 -0300

    redirecting http to https

---
+++ .platform/confighooks/postdeploy/nginx.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cat << 'EOF' > /etc/nginx/conf.d/elasticbeanstalk/99_application.conf
+location / {
+    set $redirect 0;
+
+    if ($http_x_forwarded_proto != "https") {
+        set $redirect 1;
+    }
+
+    if ($http_user_agent ~* "ELB-HealthChecker") {
+        access_log off;
+        set $redirect 0;
+    }
+
+    if ($redirect = 1) {
+        return 301 https://$host$request_uri;
+    }
+
+    proxy_pass          http://127.0.0.1:3000;
+    proxy_http_version  1.1;
+
+    proxy_set_header    Connection          $connection_upgrade;
+    proxy_set_header    Upgrade             $http_upgrade;
+    proxy_set_header    Host                $host;
+    proxy_set_header    X-Real-IP           $remote_addr;
+    proxy_set_header    X-Forwarded-For     $proxy_add_x_forwarded_for;
+}
+EOF
+
+rm /etc/nginx/conf.d/elasticbeanstalk/00_application.conf
+
+service nginx restart || service nginx start
